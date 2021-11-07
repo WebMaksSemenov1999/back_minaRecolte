ac3903e53a4311c63b0d024d08cbd6a3e186fbc1
Maks <semenov@mail.ru>
2021-11-07 16:39:08 +0300

    users resources all

--- libs/limit_offset.py
+++ libs/limit_offset.py
@@ -1,8 +1,10 @@
-def limitAndOffset(request):
+def limit_and_offset(request):
     limit = 20
     offset = 0
-    if request.args.get('limit') is not None and int(request.args.get('limit')) < limit:
-        limit = request.args.get('limit')
+    if request.args.get('limit') is not None:
+        print('is not None')
+        if int(request.args.get('limit')) < limit:
+            limit = request.args.get('limit')
     if request.args.get('offset') is not None:
         offset = request.args.get('offset')
-    return limit, offset
+    return str(limit), str(offset)
--- resources/users.py
+++ resources/users.py
@@ -1,13 +1,13 @@
 from flask_restful import Resource
 
-from libs.limit_offset import limitAndOffset
+from libs.limit_offset import limit_and_offset
 from server.users import get_users, users_get_params_query
 from flask import request
 
 
 class Users(Resource):
     def get(self):
-        limit, offset = limitAndOffset(request)
+        limit, offset = limit_and_offset(request)
         # paramsMap = users_get_params_query(request)
         params = {
             'limit': limit,
