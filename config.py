14d82473fcc878727dfe4250746391faf0ffec07
Maks <semenov@mail.ru>
2021-11-07 13:50:04 +0300

    create users get all admin

--- config.py
+++ config.py
@@ -3,4 +3,3 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/MinaRecolte"
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app import api_bp
 
 def create_app():
     app = Flask(__name__)
+    app.config['JSON_AS_ASCII'] = False
     app.register_blueprint(api_bp, url_prefix='/api')
     return app
 
