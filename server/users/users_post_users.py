1bc5d3340133ed035543dbc1c63bb60babba28eb
Maks <semenov@mail.ru>
2021-11-28 17:26:59 +0400

    min-52 registration

--- server/users/users_post_users.py
+++ server/users/users_post_users.py
@@ -1,6 +1,6 @@
 from libs.request_set import request_json_body
 from libs.validate.validate import validate
-from libs.template.validate_template import required_params, params_nik
+from libs.template.validate_template import required_params, params_nik, is_option_type_str
 
 
 def validate_params_query_all(params):
@@ -14,13 +14,17 @@ def validate_params_query_all(params):
             **params_nik,
         },
         "email": {
-
+            "regulations": {
+                **required_params,
+            }
         },
         "password": {
-
+            "regulations": {
+                **required_params,
+            }
         },
         "avatar": {
-
+            **is_option_type_str,
         }
     }
     validate(params, schema)
@@ -30,8 +34,6 @@ def validate_params_query_all(params):
 def post_users(request):
     # параметры мап
     params_request = request_json_body(request)
-    return params_request
-    # print(params_request)
     # параметры валидация
-    # validate_params_query_all(params_request)
-
+    validate_params_query_all(params_request)
+    return 1
